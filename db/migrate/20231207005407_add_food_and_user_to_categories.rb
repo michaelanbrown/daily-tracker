2ee71551ca3da0eb26d80caf613a17e68b9e9977
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-06 16:57:53 -0800

    switched references for foods

--- db/migrate/20231207005407_add_food_and_user_to_categories.rb
+++ db/migrate/20231207005407_add_food_and_user_to_categories.rb
@@ -1,6 +1,6 @@
 class AddFoodAndUserToCategories < ActiveRecord::Migration[6.1]
   def change
-    add_reference :foods, :category, foreign_key: true
+    add_reference :category, :foods, foreign_key: true
     add_reference :users, :category, foreign_key: true
   end
 end
