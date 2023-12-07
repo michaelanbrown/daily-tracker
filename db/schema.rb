af7a20f8864d5da29463d506a518c72c8bb33970
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-06 16:57:35 -0800

    rolled back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_07_005407) do
+ActiveRecord::Schema.define(version: 2023_12_07_004536) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,8 +31,6 @@ ActiveRecord::Schema.define(version: 2023_12_07_005407) do
     t.float "protein"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_foods_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,10 +41,6 @@ ActiveRecord::Schema.define(version: 2023_12_07_005407) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "category_id"
-    t.index ["category_id"], name: "index_users_on_category_id"
   end
 
-  add_foreign_key "foods", "categories"
-  add_foreign_key "users", "categories"
 end
