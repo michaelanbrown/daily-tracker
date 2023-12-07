26ae541ae5c8a72620e0d2bcf4fdf45291db5797
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-06 16:56:51 -0800

    have category reference for users

--- db/migrate/20231207005407_add_food_and_user_to_categories.rb
+++ db/migrate/20231207005407_add_food_and_user_to_categories.rb
@@ -1,5 +1,6 @@
 class AddFoodAndUserToCategories < ActiveRecord::Migration[6.1]
   def change
     add_reference :foods, :category, foreign_key: true
+    add_reference :users, :category, foreign_key: true
   end
 end
