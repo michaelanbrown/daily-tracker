57e30fdd24adf463b9e2252411e509257e19fdbf
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-06 16:59:02 -0800

    fixed typo

--- db/migrate/20231207005407_add_food_and_user_to_categories.rb
+++ db/migrate/20231207005407_add_food_and_user_to_categories.rb
@@ -1,6 +1,6 @@
 class AddFoodAndUserToCategories < ActiveRecord::Migration[6.1]
   def change
     add_reference :caategories, :food, foreign_key: true
-    add_reference :caategories, :user, foreign_key: true
+    add_reference :categories, :user, foreign_key: true
   end
 end
