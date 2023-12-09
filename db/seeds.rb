37fc79f6272ea89d4eb090ce816d4d1e2c949fc8
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-08 16:28:11 -0800

    created date #2 seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,4 @@ f3 = Food.create(name: "Turkey Pepperoni", brand: "Great Value", calories: 70, f
 u1 = User.create(name: "Michaela", age: 24, email: "michaela.brown375@gmail.com", username: "michaelanbrown", password: "owner")
 
 d1 = Date.create(date: "Today", food_id: f1.id, user_id: u1.id)
+d2 = Date.create(date: "Today", food_id: f2.id, user_id: u1.id)
