cdf52b797ecc0681a7fdcec35840a5dcd1bf1046
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-08 16:15:14 -0800

    added brand to food #2 seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 f1 = Food.create(name: "Yogurt", brand: "Friendly Farms", calories: 100, fats: 0, carbs: 8, sugars: 7, added_sugars: 2, protein: 13)
-f2 = Food.create(name: "Mashed Potatoes", calories: 110, fats: 2.5, carbs: 19, sugars: 2, added_sugars: 1, protein: 2)
+f2 = Food.create(name: "Mashed Potatoes", brand: "Idahoan", calories: 110, fats: 2.5, carbs: 19, sugars: 2, added_sugars: 1, protein: 2)
 
 u1 = User.create(name:, age:, email:, username:, password)
 
