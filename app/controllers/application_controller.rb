016c81fe7632429110b441bc0ff74cb5b8a91dab
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-09 21:42:53 -0800

    defined current_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,10 @@ class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def current_user
+    
+  end
+
   private
 
   def authenticate_user
