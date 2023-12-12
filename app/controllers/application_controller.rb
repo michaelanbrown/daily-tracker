9ad5ec773241770b471051ee956c8bb2cdd4cd78
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-11 19:03:01 -0800

    finished render_unprocessable_entity_response

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::API
   end
 
   def render_unprocessable_entity_response(exception)
-    
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
