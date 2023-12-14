eef7b59948a78f2e0ecbae6b10947bf359254714
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-14 10:37:42 -0800

    defined update method for food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -14,6 +14,9 @@ class FoodsController < ApplicationController
         render json: food, status: :created
     end
 
+    def update
+    end
+
     private
     def food_params
         params.permit(:name, :calories, :fats, :carbs, :sugars, :added_sugars, :protein, :brand)
