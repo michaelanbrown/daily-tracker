654e3cccf623f36a03466dad65cfa9afba724ec1
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-17 15:41:29 -0800

    imported header into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Header from './components/Header';
 
 function App() {
   const [users, setUsers] = useState([])
