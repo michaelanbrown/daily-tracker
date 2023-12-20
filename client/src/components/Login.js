ace921060f6bfedd2ec11bb661133a5ac350672b
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-19 17:22:09 -0800

    built first part of login submit

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,20 +6,24 @@ function Login({ }) {
     const [errors, setErrors] = useState([])
     const { currentUser, fetchCurrentUser } = useCurrentUser()
 
-    // function onSubmit(e){
-    //     e.preventDefault()
-    //     const customer = {
-    //         username,
-    //         password
-    //     }
+    function onSubmit(e){
+        e.preventDefault()
+        const customer = {
+            username,
+            password
+        }
        
-    //     fetch("/login",{
-    //       method:'POST',
-    //       headers:{'Content-Type': 'application/json'},
-    //       body:JSON.stringify(customer)
-    //     })
-        
-    // }
+        fetch("/login",{
+          method:'POST',
+          headers:{'Content-Type': 'application/json'},
+          body:JSON.stringify(customer)
+        })
+        .then(res => {
+            if (res.ok) {
+                fetchCurrentUser()
+            }
+        })
+    }
 
 
         return (
