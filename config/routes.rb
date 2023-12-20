431d2a6aec530c78139a47df2f8e05a7423ec4c2
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-19 17:26:44 -0800

    added a custom login route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
+  post "/login", to: "sessions#create" 
   get "/authorized_user", to: "customers#show"
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
