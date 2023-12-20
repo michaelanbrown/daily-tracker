20b9383346e080878dadf24a1ed9785b65589a98
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-19 17:25:20 -0800

    starting form element

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -34,7 +34,9 @@ function Login({ }) {
 
         return (
             <div>
-                
+                <form onSubmit={onSubmit}>
+                    
+                </form>
             </div>
     )
 }
