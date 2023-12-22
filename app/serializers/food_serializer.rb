5dc7c04fab81e8bdee46f34cd5a889771d7c0544
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-21 17:47:52 -0800

    replaced second fat with protein in foods serializer

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -1,5 +1,5 @@
 class FoodSerializer < ActiveModel::Serializer
-  attributes :id, :name, :calories, :fats, :carbs, :sugars, :added_sugars, :fat
+  attributes :id, :name, :calories, :fats, :carbs, :sugars, :added_sugars, :protein
   has_many :categories
   has_many :users
 end
