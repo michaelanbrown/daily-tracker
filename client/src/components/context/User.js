1b29a573b5ea04dfa772a10afd2c61d019b077a6
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-21 17:42:19 -0800

    changed from fetching customer sto fetching users

--- client/src/components/context/User.js
+++ client/src/components/context/User.js
@@ -7,7 +7,7 @@ export const CurrentUserProvider = ({ children }) => {
 
   function fetchCurrentUser() {
     fetch("/authorized_user")
-    fetch("/customers")
+    fetch("/users")
     .then((res) => {
       if(res.ok){
         res.json().then(setCurrentUser)
