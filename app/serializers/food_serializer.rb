f6b3f7c347da713474fc6954bdde8d47c78d8f63
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-21 17:46:36 -0800

    fixed foods serializer to say fats instead of fat

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -1,5 +1,5 @@
 class FoodSerializer < ActiveModel::Serializer
-  attributes :id, :name, :calories, :fat, :carbs, :suars, :added_sugars, :fat
+  attributes :id, :name, :calories, :fats, :carbs, :suars, :added_sugars, :fat
   has_many :categories
   has_many :users
 end
