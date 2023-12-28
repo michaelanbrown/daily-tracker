fca2235d891ec9f20dfb29a0d8e4fdc79123d99e
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-27 19:43:09 -0800

    added ternary operator to navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar ({ isLoading })  {
   const { currentUser, fetchCurrentUser } = useCurrentUser()
 
     return (
-      <nav>
+      isLoading ? null : <nav>
          { currentUser ? null : <NavLink className="Navelements" to="/login">Login</NavLink> }
       </nav>
     )
