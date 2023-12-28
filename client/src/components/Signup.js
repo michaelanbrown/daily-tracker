9de26c459a6add97ae5c6255cb981f614ed9a719
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-28 13:11:32 -0800

    imported useNavigate to signup

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState, useContext} from 'react'
 import { useCurrentUser } from './context/User';
 import '../App.css'
+import { useNavigate } from 'react-router-dom';
 
 function Signup({  }) {
     const { currentUser, fetchCurrentUser } = useCurrentUser()
