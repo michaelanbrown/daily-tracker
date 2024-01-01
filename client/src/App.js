963412a3bdffd9d4507ebe81f614579611d9cf09
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-31 20:00:14 -0800

    passing getCategories to login

--- client/src/App.js
+++ client/src/App.js
@@ -61,7 +61,7 @@ function App() {
       <Router>
         <Header isLoading={isLoading}/>
         <Routes>
-          <Route exact path='/login' element={<Login getUsers={getUsers} getFoods={getFoods}/>}/>
+          <Route exact path='/login' element={<Login getUsers={getUsers} getFoods={getFoods} getCategories={getCategories}/>}/>
           <Route exact path='/signup' element={<Signup getUsers={getUsers} getFoods={getFoods}/>}/>
         </Routes>
       </Router>
