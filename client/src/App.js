84d8f99c940c12000cab9ed2a5e0e6ea20b1b2ce
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-31 20:00:31 -0800

    apssing getCategories to signup

--- client/src/App.js
+++ client/src/App.js
@@ -62,7 +62,7 @@ function App() {
         <Header isLoading={isLoading}/>
         <Routes>
           <Route exact path='/login' element={<Login getUsers={getUsers} getFoods={getFoods} getCategories={getCategories}/>}/>
-          <Route exact path='/signup' element={<Signup getUsers={getUsers} getFoods={getFoods}/>}/>
+          <Route exact path='/signup' element={<Signup getUsers={getUsers} getFoods={getFoods} getCategories={getCategories}/>}/>
         </Routes>
       </Router>
     </div>
