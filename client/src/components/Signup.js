4989b6ac1c495a72e451a96d463da5328974ba0f
michaelanbrown <m_brownbrown@yahoo.com>
2023-12-31 20:01:49 -0800

    pulling props into singup

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,7 +3,7 @@ import { useCurrentUser } from './context/User';
 import '../App.css'
 import { useNavigate } from 'react-router-dom';
 
-function Signup({  }) {
+function Signup({ getUsers, getFoods, getCategories }) {
     const { currentUser, fetchCurrentUser } = useCurrentUser()
     const [errors, setErrors] = useState([])
     const [formData, setFormData] = useState({
