3f2690cd26ee7989eae5b2d6b864c73600691f72
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-03 12:09:48 -0800

    added validations for carbs

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,4 +4,5 @@ class Food < ApplicationRecord
 
   validates :calories, numericality: { greater_than: 0 }
   validates :fats, numericality: { greater_than: 0 }
+  validates :carbs, numericality: { greater_than: 0 }
 end
