0a0a3330f9beadd61ef372b2e360f4bacd7fb7f9
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-03 12:11:58 -0800

    added validations for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
     validates :age, numericality: { greater_than: 0 }
 end
