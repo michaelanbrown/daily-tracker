fed7b8e50479b36c43ba7d4b2301533b340d7384
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-04 08:42:55 -0800

    adding img tag

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,6 +5,7 @@ function Welcome({  }) {
   return (
     <div>
       <br/>
+      <img src={""} alt="" width="80%" height="80%"/>
       <h1>Food is fuel for life!</h1>
       <h3>Track your intake here for a better, healthier you.</h3>
     </div>
