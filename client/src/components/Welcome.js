74689d3d60c2340e82f94b3bc7c970d2520b7710
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-04 08:46:06 -0800

    removed h3 tag from welcome/js

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -6,7 +6,6 @@ function Welcome({  }) {
     <div>
       <br/>
       <img className="fuel" src={"https://musclenerds.net/wp-content/uploads/2020/11/Food-is-Fuel.png"} alt="Food is fuel" width="50%" height="50%"/>
-      <h3>Track your intake here for a better, healthier you.</h3>
     </div>
   );
 }
