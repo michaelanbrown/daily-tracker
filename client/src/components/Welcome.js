86b0d3b2c54e927583e9debc99d79873a3363855
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-04 08:45:00 -0800

    added fuel classname to the image

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,7 +5,7 @@ function Welcome({  }) {
   return (
     <div>
       <br/>
-      <img src={"https://musclenerds.net/wp-content/uploads/2020/11/Food-is-Fuel.png"} alt="Food is fuel" width="50%" height="50%"/>
+      <img className="fuel" src={"https://musclenerds.net/wp-content/uploads/2020/11/Food-is-Fuel.png"} alt="Food is fuel" width="50%" height="50%"/>
       <h3>Track your intake here for a better, healthier you.</h3>
     </div>
   );
