1b0bc66e945eb12656b99a22ebeaee6af53836ab
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-07 11:09:51 -0800

    brought back i nthe useeffect and usestate

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -2,11 +2,15 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function Food({ foods, setFoods }) {
+  const [foodsMap, setFoodsMap] = useState(foods)
 
-  const foodsMap = foods.map(food => {
-    <div>
-      <h4>{food.name}</h4>
-    </div>})
+  useEffect(() => {
+    setFoodsMap(foods.map(food => {
+      <div>
+        <h4>{food.name}</h4>
+      </div>
+    }))
+  },[foods])
 
   return (
     <div>
