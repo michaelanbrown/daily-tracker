ad8667d1604c6283cd98f6c4acc64ee916ae34b9
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-07 11:02:35 -0800

    built food map beginning

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function Food({ foods, setFoods }) {
   const foodMap = foods.map(food => {
-    <div></div>
+    <div>
+      <h4>{food.name}</h4>
+    </div>
   })
+
 console.log(foods)
   return (
     <div>
