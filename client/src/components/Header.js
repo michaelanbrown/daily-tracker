a181e47c4df5bbcad6fb28edef6ce6a8839a7dd7
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-07 11:17:38 -0800

    changed header to h2

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,7 +8,7 @@ function Header({ isLoading }) {
       <header>
         <div>
             <h1>🍪 Daily Tracker 🍪</h1>
-            <h3>Track your intake here for a better, healthier you.</h3>
+            <h2>Track your intake here for a better, healthier you.</h2>
             <NavBar isLoading={isLoading}/>
         </div>  
       </header>
