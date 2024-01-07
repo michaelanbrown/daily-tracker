ab7c1cca2e9a991bd269a13dceb111cc02d262a2
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-07 11:03:42 -0800

    put foodmap in the useeffect

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -2,14 +2,12 @@ import React from "react";
 import { useEffect } from "react";
 
 function Food({ foods, setFoods }) {
-  const foodMap = foods.map(food => {
-    <div>
-      <h4>{food.name}</h4>
-    </div>
-  })
-
   useEffect(() => {
-    
+    const foodMap = foods.map(food => {
+      <div>
+        <h4>{food.name}</h4>
+      </div>
+    })
   },[])
 
 console.log(foods)
