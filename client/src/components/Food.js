4e247ffb38ef1a35e63a72c165db27ff7f0e2eba
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-07 11:08:22 -0800

    removed useEffect and useState

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -2,15 +2,11 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function Food({ foods, setFoods }) {
-  const [foodsMap, setFoodsMap] = useState(foods)
 
-  useEffect(() => {
-    setFoodsMap(foods.map(food => {
-      <div>
-        <h4>{food.name}</h4>
-      </div>
-    }))
-  },[foods])
+  const foodsMap = foods.map(food => {
+    <div>
+      <h4>{food.name}</h4>
+    </div>})
 
   return (
     <div>
