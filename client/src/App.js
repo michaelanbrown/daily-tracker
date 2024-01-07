a6fd6a317e77d01ff4af7a2633d5ff99e332b85d
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-07 10:54:50 -0800

    importing foo.js to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Welcome from './components/Welcome';
+import Food from './components/Food';
 
 function App() {
   const [users, setUsers] = useState([])
