232c93aab40ae3e19d2bf7f3e6944f0cf92d242f
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-08 11:50:12 -0800

    adding navlink to home page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -24,6 +24,7 @@ export default function NavBar ({ isLoading })  {
       isLoading ? "Loading..." : <nav className='NavBar'>
         { currentUser ? null : <NavLink className="Navelements" to="/signup">Signup</NavLink> }
         { currentUser ? null : <br/> }
+        { currentUser ? <NavLink className="Navelements" to="/">Home</NavLink> : null }
         { currentUser ? null : <NavLink className="Navelements" to="/login">Login</NavLink> }
         { currentUser ? <NavLink className="Navelements" to="/food">Food</NavLink> : null }
         { currentUser ? <br/> : null }
