ac3f0b14619ac367b95eeac063d7d968a39ac7fe
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-08 11:55:22 -0800

    removed page break

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -9,7 +9,6 @@ function Food({ foods, setFoods }) {
     const mapping = foods ? setFoodsMap(foods.map(food => <div key={food.id}>
       <h4>{food.name}</h4>
       <p>{food.calories} calories</p>
-      <br/>
       <p>{food.fats}g fat</p>
       </div>)) : null
   },[foods])
