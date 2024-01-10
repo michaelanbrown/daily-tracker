b9816f37624d6dd4a11cc47d18be23dfdc6deb0f
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-10 11:07:47 -0800

    added the brand

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -8,6 +8,7 @@ function Food({ foods, setFoods }) {
   useEffect(() => {
     const mapping = foods ? setFoodsMap(foods.map(food => <div key={food.id}>
       <h4 className="foodName">{food.name}</h4>
+      <p>{food.brand}</p>
       <h4>{food.calories} calories</h4>
       <p>{food.fats}g fat</p>
       <p>{food.carbs}g carbs</p>
