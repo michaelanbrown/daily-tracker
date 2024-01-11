939b43c9cf08a382123f3085a728c365bff48d41
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-11 15:22:24 -0800

    finished handlechange foodfilter

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -25,7 +25,7 @@ function Food({ foods, setFoods }) {
   function handleChange(e) {
     setFilter(e.target.value); 
     setFoodfilter([...foods].filter(food => {
-      food.name.toLowerCase()
+      food.name.toLowerCase().indexOf(e.target.value.toLowerCase() > -1)
     }))
   }
 
