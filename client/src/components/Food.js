cf3783612d31ec7b9be9a227e3b32920dbde332e
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-13 12:35:07 -0800

    condensed the first two lines of code in Food.js

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 function Food({ foods, setFoods }) {
