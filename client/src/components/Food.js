07cf1816bf6e10b2230e8e155684974ce149e29e
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-13 12:34:44 -0800

    imported useNavigate to Food.js

--- client/src/components/Food.js
+++ client/src/components/Food.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
 
 function Food({ foods, setFoods }) {
   const [foodsMap, setFoodsMap] = useState([])
