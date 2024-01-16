8beed2bb69498ddcc9f4db80c6f074ac4754f175
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-16 08:22:33 -0800

    passing categories as a prop to today

--- client/src/App.js
+++ client/src/App.js
@@ -72,7 +72,7 @@ function App() {
           <Route exact path='/' element={<Welcome/>}/>
           <Route exact path='/food' element={<Food foods={foods} setFoods={setFoods}/>}/>
           <Route exact path='/newfood' element={<NewFood foods={foods} setFoods={setFoods}/>}/>
-          <Route exact path='/today' element={<Today users={users} foods={foods}/>}/>
+          <Route exact path='/today' element={<Today users={users} foods={foods} categories={categories}/>}/>
         </Routes>
       </Router>
     </div>
