1205256c402f70d5bae957c8805b6719c215bf57
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-17 12:41:01 -0800

    migrated new column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_12_09_001332) do
+ActiveRecord::Schema.define(version: 2024_01_17_203950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
-    t.string "date"
+    t.string "meal"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "food_id"
