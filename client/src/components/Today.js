249abceaf719008a0e38bc8a6be9f4ae8b48ba1a
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-19 10:02:28 -0800

    Added an addfood button below the snack line

--- client/src/components/Today.js
+++ client/src/components/Today.js
@@ -19,6 +19,7 @@ function Today({ users, foods, categories }) {
         <h2>Dinner</h2>
         <button className="addfood" onClick={addFood}>Add Food</button>
         <h2>Snacks</h2>
+        <button className="addfood" onClick={addFood}>Add Food</button>
     </div>
   );
 }
