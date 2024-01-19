d827c9bab623f23464189a0bd54b4cad5378f465
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-19 10:02:59 -0800

    added an extra page break below the dinner line

--- client/src/components/Today.js
+++ client/src/components/Today.js
@@ -20,6 +20,8 @@ function Today({ users, foods, categories }) {
         <br/>
         <h2>Dinner</h2>
         <button className="addfood" onClick={addFood}>Add Food</button>
+        <br/>
+        <br/>
         <h2>Snacks</h2>
         <button className="addfood" onClick={addFood}>Add Food</button>
     </div>
