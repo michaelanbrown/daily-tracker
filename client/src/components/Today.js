4e19c84247f238067209a489c541b8ffe3eb868c
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-21 17:40:56 -0800

    improted BreakfastMap into today

--- client/src/components/Today.js
+++ client/src/components/Today.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useCurrentUser } from "./context/User";
+import BreakfastMap from "./BreakfastMap";
 
 function Today({ users, foods, categories }) {
   const navigate = useNavigate()
