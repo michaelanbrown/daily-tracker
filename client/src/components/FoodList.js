717c08211e5c4fd1bf9fb298eefeb4e8db0b8616
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-29 07:39:06 -0800

    started handlechange function

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -8,6 +8,10 @@ const foodsMap = foods.map(food => {
   return <div key={food.id}>{food.name} - {food.brand}<br/><br/></div>
 })
 
+function handleChange(e) {
+
+}
+
   return (
     <div>
       <input type="text" placeholder='Search' name="filter" value={filter} className="input" onChange={handleChange} />
