11a77257507d483a4dc2cd0d7f8c4e8f187478b5
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-29 07:36:08 -0800

    removed console.log

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -2,7 +2,6 @@ import React from "react";
 
 function FoodList({ foods }) {
 //search for food or add new food
-console.log(foods)
 
 const foodsMap = foods.map(food => {
   return <div key={food.id}>{food.name} - {food.brand}<br/><br/></div>
