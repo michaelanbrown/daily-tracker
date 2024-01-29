1bf92ad6a587751b8c642595c37b51b85497e95e
michaelanbrown <m_brownbrown@yahoo.com>
2024-01-29 07:32:16 -0800

    foodsmap to map the list of foods to select from

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -2,6 +2,11 @@ import React from "react";
 
 function FoodList({ foods }) {
 //search for food or add new food
+console.log(foods)
+
+const foodsMap = foods.map(food => {
+  
+})
 
   return (
     <div>
