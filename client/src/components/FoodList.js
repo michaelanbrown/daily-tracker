8dd33bbd8d2dee3a7eb528acdb384ecd40e1fc72
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-01 16:08:30 -0800

    moved the key prop from the button to the div

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -9,7 +9,7 @@ function FoodList({ foods }) {
 
 useEffect(() => {
   const map = setFoodsMap(foodFilter.map(food => {
-    return <div><button key={food.id}>{food.name} - {food.brand}</button><br/></div>
+    return <div key={food.id}><button>{food.name} - {food.brand}</button><br/></div>
   }))
   setFoodfilter(filter ? foodFilter : foods)
 },[foods, foodFilter])
