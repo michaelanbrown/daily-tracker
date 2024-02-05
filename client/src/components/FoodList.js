4bfa05ef78129c2eaa68e623d3adf5b05a701ede
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-05 15:40:53 -0800

    if meal isn't present then it redirects you to the meal

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -20,8 +20,8 @@ function onAdd(e) {
   }
 }
 
-
 useEffect(() => {
+  const mealPresent = meal ? null : navigate("/today")
   const map = setFoodsMap(foodFilter.map(food => {
     return <div key={food.id}><button className="foodbutton" value={food.id} onClick={onAdd}>{food.name} - {food.brand}</button><br/><br/></div>
   }))
