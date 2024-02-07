f121fce17c70ff7a54cf2fd7f1f9799cbff272eb
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-07 14:52:08 -0800

    fixed snack foods to map food_id instead of category id

--- client/src/components/Today.js
+++ client/src/components/Today.js
@@ -19,7 +19,7 @@ function Today({ users, foods, categories, meal, setMeal }) {
     const breakfast = currentCategories ? setBreakfastFoods(currentFoods.filter(food => currentCategories.filter(category => category.meal === "Breakfast").map(food => food.food_id).indexOf(food.id) > -1)) : null
     const lunch = currentCategories ? setLunchFoods(currentFoods.filter(food => currentCategories.filter(category => category.meal === "Lunch").map(food => food.food_id).indexOf(food.id) > -1)) : null
     const dinner = currentCategories ? setDinnerFoods(currentFoods.filter(food => currentCategories.filter(category => category.meal === "Dinner").map(food => food.food_id).indexOf(food.id) > -1)) : null
-    const snack = currentCategories ? setSnackFoods(currentFoods.filter(food => currentCategories.filter(category => category.meal === "Snack").map(food => food.id).indexOf(food.id) > -1)) : null
+    const snack = currentCategories ? setSnackFoods(currentFoods.filter(food => currentCategories.filter(category => category.meal === "Snack").map(food => food.food_id).indexOf(food.id) > -1)) : null
   },[currentUser, currentCategories])
   
   function addFoodBreakfast() {
