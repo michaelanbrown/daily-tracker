6d82d90ad786456694b6f1fd9cb05d707dd9bf2d
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-12 16:41:25 -0800

    pulling in the midnight date of the categories created_at

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by_username(params[:username])
         if user&.authenticate(params[:password])
-          if
+          if user.categories[0].created_at.midnight
             
           end
           session[:user_id] = user.id
