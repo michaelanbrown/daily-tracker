e0f064dd04353ef99b874ee792d9494dbe4949d3
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-12 16:33:26 -0800

    removed biding.pry

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by_username(params[:username])
         if user&.authenticate(params[:password])
-          binding.pry
           session[:user_id] = user.id
           render json: user, status: :ok
         else 
