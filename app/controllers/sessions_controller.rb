62063e70995cbbfbab97550561845dee4d217c8e
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-15 17:11:28 -0800

    added byebug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
           else
             user.categories.destroy_all
           end
+          byebug
           session[:user_id] = user.id
           render json: user, status: :ok
         else 
