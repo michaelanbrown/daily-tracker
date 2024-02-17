c7fd68827db0f366acf4edf8a80f38c9cd20a031
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-17 13:08:24 -0800

    removed settingCals

--- client/src/components/Today.js
+++ client/src/components/Today.js
@@ -16,7 +16,6 @@ function Today({ users, foods, categories, meal, setMeal }) {
 
   useEffect(() => {
     const setFoods = currentUser ? setCurrentFoods(currentUser.foods) : null
-    const settingCals = currentUser ? setCals(currentFoods.map(food => food.calories).forEach()) : null
     const setCategories = currentUser ? setCurrentCategories(currentUser.categories) : null
     const breakfast = currentCategories ? setBreakfastFoods(currentFoods.filter(food => currentCategories.filter(category => category.meal === "Breakfast").map(food => food.food_id).indexOf(food.id) > -1)) : null
     const lunch = currentCategories ? setLunchFoods(currentFoods.filter(food => currentCategories.filter(category => category.meal === "Lunch").map(food => food.food_id).indexOf(food.id) > -1)) : null
