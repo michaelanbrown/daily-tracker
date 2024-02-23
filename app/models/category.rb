9687b787d45cdbe73ea64a0483b3eec29563e08a
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-22 17:06:39 -0800

    added breakfast as an inclusion option

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     belongs_to :user
     belongs_to :food
 
-    validates :meal, presence: true, inclusion: 
+    validates :meal, presence: true, inclusion: { in: %w(Breakfast) }
 end
