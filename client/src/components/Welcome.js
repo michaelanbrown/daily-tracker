54ade356f43c3e042cace814d0a03a187e75046d
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-22 17:02:28 -0800

    edited the welcome alternate text

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -5,7 +5,7 @@ function Welcome({  }) {
   return (
     <div>
       <br/>
-      <img className="fuel" src={"https://musclenerds.net/wp-content/uploads/2020/11/Food-is-Fuel.png"} alt="Food is fuel" width="50%" height="50%"/>
+      <img className="fuel" src={"https://musclenerds.net/wp-content/uploads/2020/11/Food-is-Fuel.png"} alt="Food is fuel for life" width="50%" height="50%"/>
     </div>
   );
 }
