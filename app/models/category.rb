4ff0d556ae4ac8f94dc01cc6bbffc0079f8a24f8
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-22 17:07:15 -0800

    added snack as an inclusion option

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
     belongs_to :user
     belongs_to :food
 
-    validates :meal, presence: true, inclusion: { in: %w(Breakfast Lunch Dinner) }
+    validates :meal, presence: true, inclusion: { in: %w(Breakfast Lunch Dinner Snack) }
 end
