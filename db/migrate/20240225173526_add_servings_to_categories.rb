c22bff46c61f1e7d1bc9cee5f24dc8ea4da9a2c9
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-25 09:38:57 -0800

    fixed mgration file to say add column insted of add reference

--- db/migrate/20240225173526_add_servings_to_categories.rb
+++ db/migrate/20240225173526_add_servings_to_categories.rb
@@ -1,5 +1,5 @@
 class AddServingsToCategories < ActiveRecord::Migration[6.1]
   def change
-    add_reference :categories, :servings, :float
+    add_column :categories, :servings, :float
   end
 end
