91da9406ade5ebc75b0a128a8dda1475c7a287e7
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-25 09:39:12 -0800

    migrated the new change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_17_203950) do
+ActiveRecord::Schema.define(version: 2024_02_25_173526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2024_01_17_203950) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "food_id"
     t.bigint "user_id"
+    t.float "servings"
     t.index ["food_id"], name: "index_categories_on_food_id"
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
