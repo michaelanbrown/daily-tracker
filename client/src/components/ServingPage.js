a45ec473f7d5e99b2bced01c9b89fbb6d1caed14
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-28 15:42:23 -0800

    adding onclick function to serving page

--- client/src/components/ServingPage.js
+++ client/src/components/ServingPage.js
@@ -3,6 +3,10 @@ import React from "react";
 function ServingPage({ food, onAdd, servingPage, onClick }) {
 console.log(servingPage)
 
+function onClick() {
+  setServingPage(!servingPage)
+}
+
   return (
     <div>
       <button className="foodbutton" value={food.id} onClick={onClick}>{food.name} - {food.brand}</button>
