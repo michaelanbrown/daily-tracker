85b83b507cbd46a96f0335ef935d744d694c8bdd
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-28 15:35:56 -0800

    added page break to serving page

--- client/src/components/ServingPage.js
+++ client/src/components/ServingPage.js
@@ -5,6 +5,7 @@ function ServingPage({ food, onAdd }) {
   return (
     <div>
       <button className="foodbutton" value={food.id} onClick={onAdd}>{food.name} - {food.brand}</button>
+      <br/>
     </div>
   );
 }
