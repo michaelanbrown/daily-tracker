b3e4a8725027721e20fdb61027a36865b17fdc85
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-28 15:37:25 -0800

    passing in servingPage as a prop to servingpage

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -51,7 +51,7 @@ function onAdd(e) {
 useEffect(() => {
   const mealPresent = meal ? null : navigate("/today")
   const map = setFoodsMap(foodFilter.map(food => {
-    return <ServingPage key={food.id} food={food} onAdd={onAdd}/>
+    return <ServingPage key={food.id} food={food} onAdd={onAdd} servingPage={servingPage}/>
   })) // replace onAdd
   setFoodfilter(filter ? foodFilter : foods)
 },[foods, foodFilter])
