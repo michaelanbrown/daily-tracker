b0be64d6dd6b5402aa3e31136da20e16ca35d36b
michaelanbrown <m_brownbrown@yahoo.com>
2024-02-28 15:38:38 -0800

    added onservigs function to servingspage

--- client/src/components/ServingPage.js
+++ client/src/components/ServingPage.js
@@ -2,6 +2,10 @@ import React from "react";
 
 function ServingPage({ food, onAdd, servingPage }) {
 
+  function onServings(e) {
+
+  }
+
   return (
     <div>
       <button className="foodbutton" value={food.id} onClick={onAdd}>{food.name} - {food.brand}</button>
