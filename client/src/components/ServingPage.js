8e25877630a0d7c163b5b19dc62f840a78c8c5c7
michaelanbrown <m_brownbrown@yahoo.com>
2024-03-01 16:11:33 -0800

    adding the onadd function to the servingpage

--- client/src/components/ServingPage.js
+++ client/src/components/ServingPage.js
@@ -14,6 +14,31 @@ function handleChange(e) {
   setServingSize(e.target.value)
 }
 
+function onAdd(e) {
+  e.preventDefault()
+  const mealAdd = {
+    meal: meal,
+    food_id: e.target.value,
+    user_id: currentUser.id
+  }
+  fetch(`/categories`,{
+    method:'POST',
+    headers:{'Content-Type': 'application/json'},
+    body:JSON.stringify(mealAdd)
+  })
+  .then(res => {
+    if(res.ok){
+      res.json().then(mealAdd => {
+        setCategories([...categories, mealAdd])
+        navigate('/today')
+        fetchCurrentUser()
+        })
+      } else {
+          res.json().then(json => setErrors(json.errors))
+      }
+    }) 
+  }
+
   return (
     <div>
       <button className="foodbutton" value={food.id} onClick={onClick}>{food.name} - {food.brand}</button>
