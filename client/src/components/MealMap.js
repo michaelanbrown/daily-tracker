0ccaaf25386d904cf1a046df9a7f498831e16dcc
michaelanbrown <m_brownbrown@yahoo.com>
2024-03-15 12:23:37 -0700

    set the edit button's onclick function to the onedit function

--- client/src/components/MealMap.js
+++ client/src/components/MealMap.js
@@ -34,7 +34,7 @@ function BreakfastMap({ food, category }) {
         </div>
         {food.protein === 0 ? `PRoetin: 0 g` : `Protein: ${ food.protein * category.servings } g`}
         <br/>
-        <div><button className="editDelete">Edit</button>{" "}<button className="editDelete">Delete</button></div>
+        <div><button className="editDelete" onClick={onEdit}>Edit</button>{" "}<button className="editDelete">Delete</button></div>
       </div> : null}
     </div>
   );
