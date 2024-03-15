404c881cf5abbba6731af926697f75cb00215cba
michaelanbrown <m_brownbrown@yahoo.com>
2024-03-15 12:34:46 -0700

    starting a servingInput class

--- client/src/components/MealMap.js
+++ client/src/components/MealMap.js
@@ -42,7 +42,7 @@ function BreakfastMap({ food, category }) {
       : null}
       { showMeal && edit === true ?
       <div className= "food">
-      Servings: <input type="text" placeholder='Servings' name="userservingsname" value={category.servings} className="input" />
+      Servings: <input type="text" placeholder='Servings' name="userservingsname" value={category.servings} className="servingInput" />
       <br/>
       {food.calories === 0 ? `Calories: 0` : `Calories: ${ food.calories * category.servings }`}
       <br/>
