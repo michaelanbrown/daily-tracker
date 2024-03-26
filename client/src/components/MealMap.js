56c49c7150a146166755aea93b7e8586478aae14
michaelanbrown <m_brownbrown@yahoo.com>
2024-03-26 16:54:25 -0700

    removed useNavigate import

--- client/src/components/MealMap.js
+++ client/src/components/MealMap.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
 
 function BreakfastMap({ food, category, currentCategories, setCurrentCategories }) {
   const [showMeal, setShowMeal] = useState(false)
