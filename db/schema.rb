df41a504e0139c880a8a635ece2a2dce946c18ac
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-09 17:23:34 -0700

    rolled back the date addition

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_04_10_001326) do
+ActiveRecord::Schema.define(version: 2024_02_25_173526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2024_04_10_001326) do
     t.bigint "food_id"
     t.bigint "user_id"
     t.float "servings"
-    t.datetime "date", default: Time.now.to_datetime
     t.index ["food_id"], name: "index_categories_on_food_id"
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
