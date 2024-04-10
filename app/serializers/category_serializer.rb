ddba3bb66d37e33aa4550d24c461a4ca72a33893
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-09 17:20:49 -0700

    added date to the category serizlier

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,5 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :meal, :user_id, :food_id, :servings
+  attributes :id, :meal, :user_id, :food_id, :servings, :date
 
   has_one :food
 end
