f6b88056817bae1ea2a317e884d9fd02287bd029
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-09 17:24:21 -0700

    removed the ate from thes eed file for the category

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,4 +12,4 @@ f3 = Food.create(name: "Turkey Pepperoni", brand: "Great Value", calories: 70, f
 
 u1 = User.create(name: "Michaela", age: 24, email: "michaela.brown375@gmail.com", username: "michaelanbrown", password: "owner")
 
-c1 = Category.create(meal: "Breakfast", servings: 1.5, food_id: f1.id, user_id: u1.id, date: Time.now.to_datetime)
+c1 = Category.create(meal: "Breakfast", servings: 1.5, food_id: f1.id, user_id: u1.id)
