2d5788f6dfe30aa2fbde980dd3b3594d501848e9
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-09 17:23:55 -0700

    added the default value to the migration file

--- db/migrate/20240410001326_add_date_to_categories.rb
+++ db/migrate/20240410001326_add_date_to_categories.rb
@@ -1,5 +1,5 @@
 class AddDateToCategories < ActiveRecord::Migration[6.1]
   def change
-    add_column :categories, :date, :datetime
+    add_column :categories, :date, :datetime, default: Time.now.to_datetime
   end
 end
