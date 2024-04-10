08c0b8dd5c3fc3afd025dc7d8694264c85634e26
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-09 17:24:41 -0700

    migrated the new default addition

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_25_173526) do
+ActiveRecord::Schema.define(version: 2024_04_10_001326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2024_02_25_173526) do
     t.bigint "food_id"
     t.bigint "user_id"
     t.float "servings"
+    t.datetime "date", default: "2024-04-10 00:24:29"
     t.index ["food_id"], name: "index_categories_on_food_id"
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
