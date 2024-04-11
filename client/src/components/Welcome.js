24984b1900236a15998bd51c5e5ebf25181657be
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-11 08:23:01 -0700

    mapping errors if they are present

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -8,7 +8,7 @@ function Welcome( { errors } ) {
       <img className="fuel" src={"https://musclenerds.net/wp-content/uploads/2020/11/Food-is-Fuel.png"} alt="Food is fuel for life" width="50%" height="50%"/>
       { errors ? <br/> : null }
       { errors.length !==0 ? <div className='error' >
-      
+      { errors ? errors.map(error => <div key={error}>{error}</div>) : null }
       </div> : null }
     </div>
   );
