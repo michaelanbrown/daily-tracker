3b64e9f73446d1455bf5ed480ca19fd575712f11
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-11 08:13:31 -0700

    removed useCurrentUser from foodlist

--- client/src/components/FoodList.js
+++ client/src/components/FoodList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"
-import { useCurrentUser } from "./context/User";
 import ServingPage from "./ServingPage";
 
 function FoodList({ foods, meal, categories, setCategories }) {
