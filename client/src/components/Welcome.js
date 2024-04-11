9d5e95bfe27ea58fbbcb0f8a71258ac73a2ec423
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-11 08:22:49 -0700

    adding another ternary operator for if the errors array is not 0 length

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -7,6 +7,9 @@ function Welcome( { errors } ) {
       <br/>
       <img className="fuel" src={"https://musclenerds.net/wp-content/uploads/2020/11/Food-is-Fuel.png"} alt="Food is fuel for life" width="50%" height="50%"/>
       { errors ? <br/> : null }
+      { errors.length !==0 ? <div className='error' >
+      
+      </div> : null }
     </div>
   );
 }
