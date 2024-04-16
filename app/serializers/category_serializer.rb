38880fcbb1f39f01ffb48258e36b5856bc5559de
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-16 13:53:53 -0700

    remove the date from the serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,5 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :meal, :user_id, :food_id, :servings, :date
+  attributes :id, :meal, :user_id, :food_id, :servings
 
   has_one :food
 end
