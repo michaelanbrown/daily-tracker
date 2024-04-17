136ebfbc0ccec8016771ffd78888dcd1faa0e60a
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-17 14:22:29 -0700

    added created_at to the attributes

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,5 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :meal, :user_id, :food_id, :servings
+  attributes :id, :meal, :user_id, :food_id, :servings, :created_at
 
   has_one :food
 
