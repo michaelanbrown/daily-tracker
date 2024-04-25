d5c8abc07398df7464bf65dbddadd34ad568b1b1
michaelanbrown <m_brownbrown@yahoo.com>
2024-04-25 13:28:46 -0700

    added enhanceent

--- client/src/components/Today.js
+++ client/src/components/Today.js
@@ -19,6 +19,7 @@ function Today({ setMeal, categories }) {
   const currentDate = month + " " + new Date().getDate() + ", " + new Date().getFullYear();
 
   //will need to filter categories for categories made today
+  //need to fix the rendering useEffect dependancy array
 
   useEffect(() => {
     const setFoods = currentUser ? setCurrentFoods(currentUser.foods) : null
