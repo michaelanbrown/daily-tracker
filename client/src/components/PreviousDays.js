a7e0914feaf56d6ca85b3367463222db1d553050
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-03 12:53:52 -0700

    turned the empty tag into a div

--- client/src/components/PreviousDays.js
+++ client/src/components/PreviousDays.js
@@ -79,7 +79,7 @@ function PreviousDays({ categories }) {
         Month: <select id="month" onChange={handleMonthChange}>
               {monthOptions}
               </select>
-              <>{" "}</>
+              <div>{" "}</div>
         Day: <select id="day" onChange={handleDayChange}>
               {dayOptions}
               </select>      
