db8641c82420f419a43ca77ae76ef8e265c3f362
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-06 21:01:53 -0500

    set yearsarray to an empty array

--- client/src/components/PreviousDays.js
+++ client/src/components/PreviousDays.js
@@ -14,7 +14,7 @@ function PreviousDays({ categories }) {
   const [cals, setCals] = useState(0)
   const monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const daysArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
-  const yearsArray = 
+  const yearsArray = []
   const [currentDate, setCurrentDate] = useState({
     month: '',
     day: '',
