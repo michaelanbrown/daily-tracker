412746a669df65ffe28b6eb5aff800e4f1b37d7d
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-08 15:32:41 -0700

    added the new keyword

--- client/src/components/PreviousDays.js
+++ client/src/components/PreviousDays.js
@@ -21,7 +21,7 @@ function PreviousDays({ categories }) {
     year: ''
   })
 
-  console.log(Date(). getMonth() )
+  console.log(new Date().getMonth())
 
   //edit previous days to select the date and it will populate that date's meals
 
