df05c8c785e8f64db6db83ba9434be06a4311953
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-08 15:34:38 -0700

    changed the currentDate useState month  value

--- client/src/components/PreviousDays.js
+++ client/src/components/PreviousDays.js
@@ -16,7 +16,7 @@ function PreviousDays({ categories }) {
   const daysArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
   const [yearsArray, setYearsArray] = useState([])
   const [currentDate, setCurrentDate] = useState({
-    month: '',
+    month: monthsArray[new Date().getMonth()],
     day: '',
     year: ''
   })
