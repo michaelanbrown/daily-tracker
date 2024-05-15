e699842b9f61820c81d87a20635b8b175c29c1c2
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-14 17:09:25 -0700

    changed created_at to date_value to not confused with the schema

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
     end
 
     def date
-        created_at = params[:created_at]
+        date_value = params[:created_at]
         if
             
         end
