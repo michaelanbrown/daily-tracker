85e7f58af7e1e684f8e923dcd0e080710093f8c1
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-23 16:48:19 -0700

    removed the position and margin from the class

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -13,7 +13,7 @@ function Welcome( { errors } ) {
       <br/>
       <img className="fuel" src={"https://foodisfuelllc.com/wp-content/uploads/2020/01/Food-is-Fuel_200-3.png"} alt="Food is fuel for life" width="30%" height="30%"/>
       <br/>
-      <button className="new" onClick={clickSignUp}>Signup</button>
+      <button className="welcome" onClick={clickSignUp}>Signup</button>
       { errors ? <br/> : null }
       { errors.length !==0 ? <div className='error' >
       { errors ? errors.map(error => <div key={error}>{error}</div>) : null }
