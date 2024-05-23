d78319a32a6da151efb20516d421cba4e5a06196
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-23 16:49:39 -0700

    added &nbsp; for extra space between buttons

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -14,6 +14,7 @@ function Welcome( { errors } ) {
       <img className="fuel" src={"https://foodisfuelllc.com/wp-content/uploads/2020/01/Food-is-Fuel_200-3.png"} alt="Food is fuel for life" width="30%" height="30%"/>
       <br/>
       <button className="welcome" onClick={clickSignUp}>Signup</button>
+      &nbsp;
       <button>Login</button>
       { errors ? <br/> : null }
       { errors.length !==0 ? <div className='error' >
