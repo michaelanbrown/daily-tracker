8fa0ff700ddf0e2434a38a123db98e279ff2f2bd
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-23 16:42:11 -0700

    adde page break to welcome.js

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -6,6 +6,7 @@ function Welcome( { errors } ) {
     <div>
       <br/>
       <img className="fuel" src={"https://foodisfuelllc.com/wp-content/uploads/2020/01/Food-is-Fuel_200-3.png"} alt="Food is fuel for life" width="30%" height="30%"/>
+      <br/>
       { errors ? <br/> : null }
       { errors.length !==0 ? <div className='error' >
       { errors ? errors.map(error => <div key={error}>{error}</div>) : null }
