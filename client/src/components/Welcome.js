a134700aa001d791c9a00915a05002fb728ea739
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-24 12:36:46 -0700

    added ternary operator for signup button if already logged in then no button

--- client/src/components/Welcome.js
+++ client/src/components/Welcome.js
@@ -19,7 +19,7 @@ function Welcome( { errors } ) {
       <br/>
       <img className="fuel" src={"https://foodisfuelllc.com/wp-content/uploads/2020/01/Food-is-Fuel_200-3.png"} alt="Food is fuel for life" width="30%" height="30%"/>
       <br/>
-      <button className="welcome" onClick={clickSignUp}>Signup</button>
+      { currentUser ? null : <button className="welcome" onClick={clickSignUp}>Signup</button>}
       &nbsp;
       &nbsp;
       &nbsp;
