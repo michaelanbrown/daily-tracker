8c189aa2dcea4db3c8c26e4c4695158343a84074
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-27 16:52:57 -0700

    passing setIsLoading to header

--- client/src/App.js
+++ client/src/App.js
@@ -69,7 +69,7 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Header isLoading={isLoading}/>
+        <Header isLoading={isLoading} setIsLoading={setIsLoading}/>
         <Routes>
           <Route exact path='/login' element={<Login getUsers={getUsers} getFoods={getFoods} getCategories={getCategories} setIsLoading={setIsLoading}/>}/>
           <Route exact path='/signup' element={<Signup getUsers={getUsers} getFoods={getFoods} getCategories={getCategories} users={users} setUsers={setUsers} setIsLoading={setIsLoading}/>}/>
