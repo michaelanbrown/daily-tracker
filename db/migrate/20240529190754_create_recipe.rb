ba40dc566367a59a5448b45ede40d6ecfc9d5670
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-29 12:13:06 -0700

    Added defulat to 0 for recipe carbs

--- db/migrate/20240529190754_create_recipe.rb
+++ db/migrate/20240529190754_create_recipe.rb
@@ -4,7 +4,7 @@ class CreateRecipe < ActiveRecord::Migration[6.1]
       t.string :name
       t.float :calories, default: 0
       t.float :fats, default: 0
-      t.float :carbs
+      t.float :carbs, default: 0
       t.float :sugars
       t.float :added_sugars
       t.float :protein
