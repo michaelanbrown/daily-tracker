8e685e62c2444e1a782fd04677623c447362dee7
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-29 12:08:04 -0700

    generated a recipe migration

---
+++ db/migrate/20240529190754_create_recipe.rb
@@ -0,0 +1,15 @@
+class CreateRecipe < ActiveRecord::Migration[6.1]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.float :calories
+      t.float :fats
+      t.float :carbs
+      t.float :sugars
+      t.float :added_sugars
+      t.float :protein
+
+      t.timestamps
+    end
+  end
+end
