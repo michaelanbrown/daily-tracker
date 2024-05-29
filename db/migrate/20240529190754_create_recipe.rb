6a967a6515d5440f4f4fa799ba9bcd7fb7597a3f
michaelanbrown <m_brownbrown@yahoo.com>
2024-05-29 12:12:46 -0700

    Added defulat to 0 for recipe calories

--- db/migrate/20240529190754_create_recipe.rb
+++ db/migrate/20240529190754_create_recipe.rb
@@ -2,7 +2,7 @@ class CreateRecipe < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
       t.string :name
-      t.float :calories
+      t.float :calories, default: 0
       t.float :fats
       t.float :carbs
       t.float :sugars
