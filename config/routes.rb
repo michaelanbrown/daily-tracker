d89ac482097fe034130353ac0a43bf318f3686c9
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-04 14:48:42 -0700

    edited routes.rb for ingredients to only have a route to the index and create methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  resources :ingredients
+  resources :ingredients, only: [:index, :create]
   resources :recipes, only: [:index, :show, :create]
   resources :users, only: [:index, :show, :create]
   resources :foods
