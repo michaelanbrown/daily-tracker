ec380d39f4fe4acd44cd9ca61794b6cd00e2eef3
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-06 15:13:04 -0500

    adding a page break to the navbar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -34,6 +34,7 @@ export default function NavBar ({ isLoading })  {
         { currentUser ? <NavLink className="Navelements" to="/food">Food</NavLink> : null }
         { currentUser ? <br/> : null }
         { currentUser ? <NavLink className="Navelements" to="/recipes">Recipes</NavLink> : null }
+        { currentUser ? <br/> : null }
         { currentUser ? <button className="logout" onClick={handleLogout}>Logout</button> : null }
       </nav>
     )
