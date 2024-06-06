4a5b88cb00181280974da4fd1edca70d4fce2bd7
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-06 15:15:45 -0500

    added comments about future functionality for recipeshow

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -1,6 +1,8 @@
 import React from "react";
 
 function RecipeShow(  ) {
+  //want to be able to edit the recipe
+    //add a new ingredient or be able to edit ingredient serving size
 
   return (
     <div>
