bfaddb3525b1f8beb0065444cc93dfcab1a77a7f
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-06 15:07:13 -0500

    taking in getRecipes as a prop to signup

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -3,7 +3,7 @@ import { useCurrentUser } from './context/User';
 import '../App.css'
 import { useNavigate } from 'react-router-dom';
 
-function Signup({ getUsers, getFoods, getCategories, users, setUsers, setIsLoading }) {
+function Signup({ getUsers, getFoods, getRecipes, getCategories, users, setUsers, setIsLoading }) {
     const { currentUser, fetchCurrentUser } = useCurrentUser()
     const [errors, setErrors] = useState([])
     const [formData, setFormData] = useState({
