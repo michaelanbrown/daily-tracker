55a464b9be4f6178f64d81a6898674b0042d5638
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-10 17:12:30 -0500

    taking in ingredients as a prop in the ingredients page

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Ingredients(  ) {
+function Ingredients( { ingredients } ) {
     //will want to be able to edit ingredient serving size
     //will be able to see all ingredients and click into each one
 
