3e1ad98d1c52cd25a8659feb81d86e384531de16
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-10 17:07:13 -0500

    passing getIngredients to the signup page

--- client/src/App.js
+++ client/src/App.js
@@ -96,7 +96,7 @@ function App() {
         <Header isLoading={isLoading}/>
         <Routes>
           <Route exact path='/login' element={<Login getUsers={getUsers} getFoods={getFoods} getRecipes={getRecipes} getIngredients={getIngredients} getCategories={getCategories} setIsLoading={setIsLoading}/>}/>
-          <Route exact path='/signup' element={<Signup getUsers={getUsers} getFoods={getFoods} getRecipes={getRecipes} getCategories={getCategories} users={users} setUsers={setUsers} setIsLoading={setIsLoading}/>}/>
+          <Route exact path='/signup' element={<Signup getUsers={getUsers} getFoods={getFoods} getRecipes={getRecipes} getIngredients={getIngredients} getCategories={getCategories} users={users} setUsers={setUsers} setIsLoading={setIsLoading}/>}/>
           <Route exact path='/' element={<Welcome errors={errors} isLoading={isLoading}/>}/>
           <Route exact path='/food' element={<Food foods={foods}/>}/>
           <Route exact path='/newfood' element={<NewFood foods={foods} setFoods={setFoods}/>}/>
