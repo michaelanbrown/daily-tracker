bd1aa6ad21fbd76db3d75badd1d856771171576d
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-10 17:11:03 -0500

    added a s=comment for what the ingredients page will include

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Ingredients(  ) {
     //will want to be able to edit ingredient serving size
+    //will be able to see all ingredients and click into each one
 
   return (
     <div>
