dcfa51f1b3558198cab4624649b497e0b4385ee9
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-13 16:56:07 -0700

    started a recipeMap constant to map all of the recipes

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -4,6 +4,7 @@ import RecipeShow from "./RecipeShow";
 function Recipes( { recipes } ) {
     //want to be able to click into a recipe
     //want to be able to create a new recipe
+    const recipeMap
 
   return (
     <div>
