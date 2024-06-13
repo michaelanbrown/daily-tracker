f8787ac64d44bf5ddf95bfac7cc52bbd583daa59
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-13 16:57:17 -0700

    imported navlink into recipes

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -1,11 +1,12 @@
 import React from "react";
 import RecipeShow from "./RecipeShow";
+import { NavLink } from "react-router-dom"
 
 function Recipes( { recipes } ) {
     //want to be able to click into a recipe
     //want to be able to create a new recipe
     const recipeMap = recipes.map(recipe => {
-      
+
     })
 
   return (
