eed77bab0ba957d1bc715dfdbb30a199b69d115f
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-14 13:28:42 -0700

    set the ingredientMap constant to a map of the ingredients

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -5,7 +5,9 @@ function Ingredients( { ingredients } ) {
     //will want to be able to edit ingredient serving size
     //will be able to see all ingredients and click into each one
 
-    const ingredientMap
+    const ingredientMap = ingredients.map(
+      
+    )
 
   return (
     <div>
