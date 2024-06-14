7fc21ce689355137a5a351ebacc746f7d2391bb5
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-14 13:29:30 -0700

    started to conclude that the return value will include a div tag

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -5,9 +5,11 @@ function Ingredients( { ingredients } ) {
     //will want to be able to edit ingredient serving size
     //will be able to see all ingredients and click into each one
 
-    const ingredientMap = ingredients.map(
-      
-    )
+    const ingredientMap = ingredients.map( ingredient => {
+      return <div>
+        
+      </div>
+    })
 
   return (
     <div>
