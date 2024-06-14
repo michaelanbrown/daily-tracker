b2dcff9f9237ec572f0a003334485f38c7580ac8
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-13 17:12:15 -0700

    added a link to the recipe route

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -6,9 +6,11 @@ function Recipes( { recipes } ) {
     //want to be able to click into a recipe
     //want to be able to create a new recipe
     const recipeMap = recipes.map(recipe => {
-      return <><Routes key={recipe.id}>
-      <Route path={`recipes/${recipe.id}`} element={<RecipeShow/>}/>
-        </Routes></>
+      return <>
+      <Link to={`${recipe.id}`}></Link>
+      <Routes key={recipe.id}>
+        <Route path={`recipes/${recipe.id}`} element={<RecipeShow/>}/>
+      </Routes></>
     })
 
   return (
