7e3b50b0ef9b95eaf91c2c9f580a412f1932a79d
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-13 17:09:54 -0700

    nested the route in a routes

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -6,7 +6,9 @@ function Recipes( { recipes } ) {
     //want to be able to click into a recipe
     //want to be able to create a new recipe
     const recipeMap = recipes.map(recipe => {
-      return <Route key={recipe.id} path={`recipes/${recipe.id}`} element={<RecipeShow/>}/>
+      return <Routes>
+      <Route key={recipe.id} path={`recipes/${recipe.id}`} element={<RecipeShow/>}/>
+        </Routes>
     })
 
   return (
