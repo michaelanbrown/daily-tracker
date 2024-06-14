fe18f0f499f48066d4f30224a4f388999bf6fb9a
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-14 13:28:14 -0700

    began and ingredientMap constant

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -5,6 +5,8 @@ function Ingredients( { ingredients } ) {
     //will want to be able to edit ingredient serving size
     //will be able to see all ingredients and click into each one
 
+    const ingredientMap
+
   return (
     <div>
       
