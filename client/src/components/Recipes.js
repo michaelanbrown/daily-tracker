842f433333cc83e164f510bef0d31618ee82bab3
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-13 17:13:49 -0700

    added the key to the div tag

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -6,9 +6,9 @@ function Recipes( { recipes } ) {
     //want to be able to click into a recipe
     //want to be able to create a new recipe
     const recipeMap = recipes.map(recipe => {
-      return <div>
+      return <div key={recipe.id}>
       <Link to={`${recipe.id}`}>{recipe.name}</Link>
-      <Routes key={recipe.id}>
+      <Routes>
         <Route path={`recipes/${recipe.id}`} element={<RecipeShow/>}/>
       </Routes></div>
     })
