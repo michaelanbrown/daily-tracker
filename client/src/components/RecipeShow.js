39e3436081773dbe4c0daf195ae56b6adbfcff03
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-17 10:50:19 -0500

    took recipe into the recipeshow page

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -1,7 +1,7 @@
 import React from "react";
 import IngredientShow from "./IngredientShow";
 
-function RecipeShow(  ) {
+function RecipeShow( { recipe } ) {
   //want to be able to edit the recipe
     //add a new ingredient or be able to edit ingredient serving size
 
