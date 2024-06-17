71e3270411d6081252b0dafc448cba6ed3bbe9aa
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-17 10:49:08 -0500

    erased recipe comment for functionality that has been built

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -3,8 +3,8 @@ import RecipeShow from "./RecipeShow";
 import { Route, Routes, Link } from "react-router-dom";
 
 function Recipes( { recipes } ) {
-    //want to be able to click into a recipe
     //want to be able to create a new recipe
+    
     const recipeMap = recipes.map(recipe => {
       return <div key={recipe.id}>
       <Link to={`${recipe.id}`}>{recipe.name}</Link>
