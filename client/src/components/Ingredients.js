98ac0ab52b2cbe248ba8bef23ab829387b23c9ea
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-17 10:48:52 -0500

    erased ingredient comment for functionality that has been built

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -4,7 +4,6 @@ import { Route, Routes, Link } from "react-router-dom";
 
 function Ingredients( { ingredients } ) {
     //will want to be able to edit ingredient serving size
-    //will be able to see all ingredients and click into each one
 
     const ingredientMap = ingredients.map(ingredient => {
       return <div key={ingredient.id}>
