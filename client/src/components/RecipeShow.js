2cf4b0e24bfc9a5c9d1b27eeb277e7be0e939fd3
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-18 16:51:34 -0700

    started a useEffect in recipeshow to grab the recipe information

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -6,6 +6,11 @@ function RecipeShow( { recipes } ) {
   //want to be able to edit the recipe
     //add a new ingredient or be able to edit ingredient serving size
 
+    useEffect(() => {
+      
+    },)
+
+
   return (
     <div>
       
