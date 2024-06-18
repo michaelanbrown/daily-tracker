b93cd7cf6de83ca6336490cc64f315f62bb9023d
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-18 16:49:08 -0700

    removed console.log

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -4,7 +4,7 @@ import IngredientShow from "./IngredientShow";
 function RecipeShow( { recipes, recipe } ) {
   //want to be able to edit the recipe
     //add a new ingredient or be able to edit ingredient serving size
-    console.log(recipe)
+
   return (
     <div>
       
