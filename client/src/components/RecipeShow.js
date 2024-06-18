1b52dd919aed65f07b3405ff558e0cd466e51a52
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-18 16:56:56 -0700

    started a recipe constant

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 
 function RecipeShow( { recipes } ) {
   const { id } = useParams()
+  const [recipe, setRecipe]
   //want to be able to edit the recipe
     //add a new ingredient or be able to edit ingredient serving size
 
