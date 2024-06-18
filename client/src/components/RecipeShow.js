f5628ea6ea1fdfbcccdcc2507947c46b4a8d4b0e
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-18 16:57:19 -0700

    set the recipe constant to an empty object

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 
 function RecipeShow( { recipes } ) {
   const { id } = useParams()
-  const [recipe, setRecipe]
+  const [recipe, setRecipe] = useState({})
   //want to be able to edit the recipe
     //add a new ingredient or be able to edit ingredient serving size
 
