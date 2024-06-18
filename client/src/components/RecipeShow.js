e1bd11be96ef897a78e658634b7749fa8daf565f
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-18 16:54:51 -0700

    imported useParams into recipeshow

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -1,6 +1,7 @@
 import React from "react";
 import IngredientShow from "./IngredientShow";
 import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
 
 function RecipeShow( { recipes } ) {
   //want to be able to edit the recipe
