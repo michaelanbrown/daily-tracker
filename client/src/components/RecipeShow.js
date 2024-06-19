e9a89f06a96aa4e3077e4260c00460413349bea9
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-18 17:00:22 -0700

    set the errors constant to an empty useState array

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -6,7 +6,7 @@ import { useParams } from "react-router-dom";
 function RecipeShow( { recipes } ) {
   const { id } = useParams()
   const [recipe, setRecipe] = useState({})
-  const [errors, setErrors]
+  const [errors, setErrors] = useState([])
   //want to be able to edit the recipe
     //add a new ingredient or be able to edit ingredient serving size
 
