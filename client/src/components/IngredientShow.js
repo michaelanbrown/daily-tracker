a81442fc027a6161e0e26ab4f30d0f46ac80b434
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-23 13:44:21 -0500

    imported usestate to ingredientshow

--- client/src/components/IngredientShow.js
+++ client/src/components/IngredientShow.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function IngredientShow( { ingredients, ingredient } ) {
     //will want to be able to edit ingredient serving size
