56916214294b525bae8b61297ee6d0e455710723
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-23 13:44:40 -0500

    imported useParams into ingredientshow

--- client/src/components/IngredientShow.js
+++ client/src/components/IngredientShow.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
 
 function IngredientShow( { ingredients, ingredient } ) {
     //will want to be able to edit ingredient serving size
