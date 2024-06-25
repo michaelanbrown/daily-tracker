d7d3f5943087f6378e7ba53f14f1ef4975e2ac9f
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-25 11:33:46 -0500

    pulling setIngredients into the ingredientShow page

--- client/src/components/IngredientShow.js
+++ client/src/components/IngredientShow.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 
-function IngredientShow( { ingredients } ) {
+function IngredientShow( { ingredients, setIngredients } ) {
     //will want to be able to edit ingredient serving size
     const { id } = useParams()
     const [errors, setErrors] = useState([])
