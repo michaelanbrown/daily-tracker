8298f0a0e421db5a87dfbff9628f22880cb5de05
michaelanbrown <m_brownbrown@yahoo.com>
2024-06-28 08:58:26 -0500

    added a comment to recipeShow about wanting to have a link to each ingredient

--- client/src/components/RecipeShow.js
+++ client/src/components/RecipeShow.js
@@ -10,6 +10,7 @@ function RecipeShow( { recipes, setRecipes } ) {
   const [edit, setEdit] = useState(false)
   //want to be able to edit the recipe - the function for a PATCH request has been created
     //add a new ingredient or be able to edit ingredient serving size - this is going to be accomplished in the ingredientshow page
+    //need to be able to access all ingredients in a recipe and to be able to click a link to get to each ingredient
 
     useEffect(() => {
       fetch(`${id}`)
