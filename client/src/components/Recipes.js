b75244a315d070cc78e62b3a5a268f2243ee319a
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-02 09:42:37 -0500

    if the response is not ok then we are going to set our errors constant

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -31,7 +31,7 @@ function Recipes( { recipes, setRecipes } ) {
           navigate(`/recipes`)
         })
       } else {
-        
+        res.json().then(json => setErrors(json.errors))
       }
     })
     }
