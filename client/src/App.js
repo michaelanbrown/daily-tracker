8ff146f89cb9bc8e13da879f2e4e99d1f0920b15
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-02 09:36:08 -0500

    passing setRecipes from app.js to the recipes page

--- client/src/App.js
+++ client/src/App.js
@@ -107,7 +107,7 @@ function App() {
           <Route exact path='/today' element={<Today setMeal={setMeal} categories={categories}/>}/>
           <Route exact path='/previousdays' element={<PreviousDays categories={categories}/>}/>
           <Route exact path='/foodlist' element={<FoodList foods={foods} meal={meal} categories={categories} setCategories={setCategories}/>}/>
-          <Route exact path='/recipes/*' element={<Recipes recipes={recipes}/>}/>
+          <Route exact path='/recipes/*' element={<Recipes recipes={recipes} setRecipes={setRecipes}/>}/>
           <Route exact path="/recipes/:id" element={<RecipeShow recipes={recipes} setRecipes={setRecipes}/>}/>
           <Route exact path='/ingredients/*' element={<Ingredients ingredients={ingredients}/>}/>
           <Route exact path="/ingredients/:id" element={<IngredientShow ingredients={ingredients} setIngredients={setIngredients}/>}/>
