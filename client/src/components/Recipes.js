5242665dd9a12d4f482f8189f385d10d54ebcc69
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-03 08:43:57 -0500

    removed the add recipe function from the recipes page

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -6,33 +6,6 @@ function Recipes( { recipes, setRecipes } ) {
   const navigate = useNavigate();
     //want to be able to create a new recipe - put newRecipe function on a new page
 
-    function addRecipe(e) {
-      e.preventDefault();
-      const recipe = {
-        name,
-        calories,
-        fats,
-        carbs,
-        sugars,
-        added_sugars,
-        protein
-      }
-      fetch(`/recipes`, {
-       method: 'POST',
-       headers: {
-        "Content-Type" : "application/json"
-       },
-      body: JSON.stringify(recipe)
-    }).then(res => {
-      if (res.ok) {
-        res.json().then(recipe => {
-          setRecipes([...recipes, recipe])
-          navigate(`/recipes`)
-        })
-      } else {
-        res.json().then(json => setErrors(json.errors))
-      }}
-    )}
     
     const recipeMap = recipes.map(recipe => {
       return <div key={recipe.id}>
