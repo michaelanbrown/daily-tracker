674c496889cc3d3aa84e597395a081c4c55bc7e7
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-03 08:44:15 -0500

    put the addrecipe function onto the new recipe page

--- client/src/components/newRecipe.js
+++ client/src/components/newRecipe.js
@@ -3,6 +3,34 @@ import React from "react";
 function NewRecipe( { recipes, setRecipes } ) {
     const [errors, setErrors] = useState([])
 
+    function addRecipe(e) {
+        e.preventDefault();
+        const recipe = {
+          name,
+          calories,
+          fats,
+          carbs,
+          sugars,
+          added_sugars,
+          protein
+        }
+        fetch(`/recipes`, {
+         method: 'POST',
+         headers: {
+          "Content-Type" : "application/json"
+         },
+        body: JSON.stringify(recipe)
+      }).then(res => {
+        if (res.ok) {
+          res.json().then(recipe => {
+            setRecipes([...recipes, recipe])
+            navigate(`/recipes`)
+          })
+        } else {
+          res.json().then(json => setErrors(json.errors))
+        }}
+      )}
+
   return (
     <div>
       <button className="new" onClick={}>Add New Recipe</button>
