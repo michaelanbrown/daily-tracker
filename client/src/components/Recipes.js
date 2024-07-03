1deff38efaa91d4f4c88c92c62da57b9a2fba2cc
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-03 08:49:23 -0500

    re-defined the navigate constant

--- client/src/components/Recipes.js
+++ client/src/components/Recipes.js
@@ -4,7 +4,7 @@ import { Route, Routes, Link, useNavigate } from "react-router-dom";
 
 function Recipes( { recipes, setRecipes } ) {
     //want to be able to create a new recipe - put newRecipe function on a new page
-
+    const navigate = useNavigate();
     
     const recipeMap = recipes.map(recipe => {
       return <div key={recipe.id}>
