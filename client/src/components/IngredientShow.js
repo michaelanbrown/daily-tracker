043585db742012008200728718a783a249c7215a
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-11 10:29:03 -0500

    removed comment from ingredientShow page

--- client/src/components/IngredientShow.js
+++ client/src/components/IngredientShow.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 
 function IngredientShow( { ingredients, setIngredients } ) {
-    //will want to be able to edit ingredient serving size - function is complete
     const { id } = useParams()
     const [errors, setErrors] = useState([])
     const [ingredient, setIngredient] = useState({})
