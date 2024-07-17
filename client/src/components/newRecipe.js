12e6b4ca5d0112b6b8c52633e8fedb4cfe12c369
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-17 18:46:22 -0500

    added a page break after the protein input tag

--- client/src/components/newRecipe.js
+++ client/src/components/newRecipe.js
@@ -66,6 +66,7 @@ function NewRecipe( { recipes, setRecipes } ) {
         <input type="text" placeholder='Added Sugars' name="added_sugars" value={added_sugars} className="input" onChange={handleChange}/>
           <br/>
         <input type="text" placeholder='Protein' name="protein" value={protein} className="input" onChange={handleChange}/>
+          <br/>
       </form>
     </div>
   );
