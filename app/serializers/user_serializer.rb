5f206fc4c2550e92b7c58a5edf2bdf72062dfbca
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-18 18:58:40 -0500

    add has_many :foods to the users serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :email, :username, :password_digest, :categories, :foods
   has_many :categories
+  has_many :foods
 end
