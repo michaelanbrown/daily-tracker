f6925611d7ad80ba65663614d0969574ff7a82da
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-18 19:00:46 -0500

    added has_many :ingredients to the food serializer

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -2,4 +2,5 @@ class FoodSerializer < ActiveModel::Serializer
   attributes :id, :name, :calories, :fats, :carbs, :sugars, :added_sugars, :protein, :brand, :categories
   has_many :categories
   has_many :users
+  has_many :ingredients
 end
