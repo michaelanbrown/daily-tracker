77aefa9a6f676cad2bec8aec1c7a17d829174722
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-23 18:14:53 -0700

    added an enhancement comment to find the ingredient name

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -3,6 +3,7 @@ import IngredientShow from "./IngredientShow";
 import { Route, Routes, Link } from "react-router-dom";
 
 function Ingredients( { ingredients, foods } ) {
+  //use foods to find the food name for each ingredient
 
     const ingredientMap = ingredients.map(ingredient => {
       return <div key={ingredient.id}>
