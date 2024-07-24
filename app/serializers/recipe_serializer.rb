65216cefd27caed4e274b41aa5153c7b925f914f
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-23 18:10:18 -0700

    removed associations from recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,3 @@
 class RecipeSerializer < ActiveModel::Serializer
   attributes :id, :name, :calories, :fats, :carbs, :sugars, :added_sugars, :protein, :ingredients
-  has_many :foods
-  has_many :ingredients
 end
