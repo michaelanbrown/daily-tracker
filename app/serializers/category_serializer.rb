b301a257c729c4891bc4752a49eb5b21ff072b30
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-23 18:12:08 -0700

    removed association from catgory serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,7 +1,6 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :meal, :user_id, :food_id, :servings, :created_at
 
-  has_one :food
 
   def created_at
     object.created_at.strftime("%B %d, %Y")
