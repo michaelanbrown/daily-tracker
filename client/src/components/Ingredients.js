1b38495a45777aa1c54a5470548735fa49bfd6c2
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-24 20:14:00 -0500

    putting the food filtering in the link

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -7,7 +7,7 @@ function Ingredients( { ingredients, foods } ) {
 
     const ingredientMap = ingredients.map(ingredient => {
       return <div key={ingredient.id}>
-        <Link to={`${ingredient.id}`}>{ingredient.name}</Link>
+        <Link to={`${ingredient.id}`}>{foods.filter()}</Link>
         <Routes>
           <Route path={`ingredients/${ingredient.id}`} element={<IngredientShow ingredients={ingredients}/>}/>
         </Routes>
