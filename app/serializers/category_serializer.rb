0ae045c5aa04d95623234237323e1f957fdb8261
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-31 20:16:26 -0500

    also added the attributes for the associations to the category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,5 +1,5 @@
 class CategorySerializer < ActiveModel::Serializer
-  attributes :id, :meal, :user_id, :food_id, :servings, :created_at
+  attributes :id, :meal, :user_id, :food_id, :servings, :created_at, :user, :food
 
   belongs_to :user
   belongs_to :food
