0325fd26600415e6a0796445793a777c1a6c0cae
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-31 20:16:52 -0500

    added the categories association to the food serializer

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -1,3 +1,5 @@
 class FoodSerializer < ActiveModel::Serializer
   attributes :id, :name, :calories, :fats, :carbs, :sugars, :added_sugars, :protein, :brand, :categories
+
+  has_many :categories
 end
