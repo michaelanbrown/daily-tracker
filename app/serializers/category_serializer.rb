efc4969e486e29a5f2f79577db5aadf24e6effaf
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-31 20:15:34 -0500

    adding associations back to the category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,6 +1,7 @@
 class CategorySerializer < ActiveModel::Serializer
   attributes :id, :meal, :user_id, :food_id, :servings, :created_at
 
+  belongs_to :user
 
   def created_at
     object.created_at.strftime("%B %d, %Y")
