6f8c08f411a62eedc9e936d030cc2b6effd58c0c
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-31 20:17:28 -0500

    added the users association and attribute to the food serializer

--- app/serializers/food_serializer.rb
+++ app/serializers/food_serializer.rb
@@ -1,5 +1,6 @@
 class FoodSerializer < ActiveModel::Serializer
-  attributes :id, :name, :calories, :fats, :carbs, :sugars, :added_sugars, :protein, :brand, :categories
+  attributes :id, :name, :calories, :fats, :carbs, :sugars, :added_sugars, :protein, :brand, :categories, :users
 
   has_many :categories
+  has_many :users
 end
