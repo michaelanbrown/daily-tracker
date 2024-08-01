29933285665e410267c50236d1b0acf732d7750d
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-31 20:15:57 -0500

    added food assocation back to category serializer

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -2,6 +2,7 @@ class CategorySerializer < ActiveModel::Serializer
   attributes :id, :meal, :user_id, :food_id, :servings, :created_at
 
   belongs_to :user
+  belongs_to :food
 
   def created_at
     object.created_at.strftime("%B %d, %Y")
