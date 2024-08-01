13d6552996ebfed2e2518bd414ecd397900fb9c9
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-31 20:18:19 -0500

    also added the food and recipe to the ingredient serializer attributes list

--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,5 +1,5 @@
 class IngredientSerializer < ActiveModel::Serializer
-  attributes :id, :serving_size, :food_id, :recipe_id
+  attributes :id, :serving_size, :food_id, :recipe_id, :food, :recipe
 
   belongs_to :food
   belongs_to :recipe
