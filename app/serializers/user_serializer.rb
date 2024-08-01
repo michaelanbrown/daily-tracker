8a167823050dff3aa60e7aac58d527d1a721f34f
michaelanbrown <m_brownbrown@yahoo.com>
2024-07-31 20:19:36 -0500

    added a space o the user serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :age, :email, :username, :password_digest, :categories, :foods
+  
   has_many :categories
   has_many :foods
 end
